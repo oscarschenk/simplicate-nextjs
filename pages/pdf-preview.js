b6ddfe3f9d19fe33b86f873fa42adcdc80ee66ec
Oscar Schenk <schenkoscar@gmail.com>
2022-05-17 11:32:53 +0200

    fixed max width pdf preview

--- pages/pdf-preview.js
+++ pages/pdf-preview.js
@@ -6,17 +6,6 @@ import Link from "next/link";
 import { data } from "../public/data";
 
 export default function pdfPreview(toggled) {
-  const previewItems = data.map((item) => {
-    return (
-      <div
-        key={item.dienst}
-        className="py-4 px-8 rounded-lg transition ease-in-out delay-20 bg-sky-500 hover:-translate-y-1 hover:scale-105 hover:bg-violet-400 duration-300 cursor-pointer shadow-2xl flex flex-row justify-between"
-      >
-        <p>{item.dienst}</p>
-        <Switch size="sm" color="warning" />
-      </div>
-    );
-  });
   return (
     <div>
       <Head>
@@ -26,7 +15,7 @@ export default function pdfPreview(toggled) {
       </Head>
 
       <div className="flex items-center justify-center h-screen w-screen">
-        <div className="bg-gradient-to-br from-gray-100 to-white w-5/6 py-20 px-8 rounded-xl shadow-2xl h-5/6 text-white flex items-center justify-center flex-col overflow-y-auto">
+        <div className="bg-gradient-to-br from-gray-100 to-white w-5/6 max-w-screen-xl py-20 px-8 rounded-xl shadow-2xl h-5/6 text-white flex items-center justify-center flex-col overflow-y-auto">
           <h1 className="text-gray-600 text-3xl">PDF Data Preview</h1>
           <div className="w-full h-full">
             <motion.div
